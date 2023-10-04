775c4df2e021d7bf43d8cbb9c4bf8ab43aec93ad
j.kendall.reid@gmail.com <j.kendall.reid@gmail.com>
2023-10-04 12:14:33 +0000

    Created custom package dependencies; Edited LICENCE, settings, DatabaseProxy and SearchHintsDemo

--- client_code/LICENCE.py
+++ client_code/LICENCE.py
@@ -1,3 +1,6 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 """
                                  Apache License
                            Version 2.0, January 2004
